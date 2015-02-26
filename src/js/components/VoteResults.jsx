f854d2e72669d1349fb1a249701818cc470d3d13
Benjamin Eberlei <kontakt@beberlei.de>
2015-02-26 21:05:44 +0100

    Add numer of votes.

--- src/js/components/VoteResults.jsx
+++ src/js/components/VoteResults.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 module.exports = React.createClass({
     renderVote: function(vote, idx) {
         var share = Math.round(vote.share * 100, 2);
-        return <li>{vote.option} ({share}%)</li>;
+        return <li>{vote.option}: {vote.votes} ({share}%)</li>;
     },
     computeTotalVotesCasted: function() {
         var total = 0;
