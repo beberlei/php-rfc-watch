de20dc98ab4f4880096a803cec0afec60290c1a7
Benjamin Eberlei <kontakt@beberlei.de>
2018-07-12 11:45:30 +0200

    Improve layout a little.

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,7 +31,7 @@ class VoteResults extends React.Component {
         return (
             <div>
                 <div className="mb-2">
-                    <strong>{this.props.question}</strong>
+                    {this.props.question}
                 </div>
 
                 <div className="vote-results mb-2">
@@ -42,6 +42,8 @@ class VoteResults extends React.Component {
                     <div className="col-lg">Votes cast: {this.computeTotalVotesCasted()}</div>
                     {this.props.results.map(this.renderVote)}
                 </div>
+
+                {this.props.last ? null : <hr />}
             </div>
         );
     }
@@ -49,17 +51,21 @@ class VoteResults extends React.Component {
 
 class RfcVoteItem extends React.Component {
     render() {
-        return <div className="card">
-            <div className="card-header">
-                {this.props.rfc.status == 'open' ?
-                    <span className="badge badge-primary mr-1">Active</span>
-                    : null }
-                <a href={this.props.rfc.url} target="_blank">{this.props.rfc.title}</a>
-            </div>
-            <div className="card-body">
-                {this.props.rfc.questions.map(item => {
-                    return <VoteResults question={item.question} results={item.results} share={item.share} />
-                })}
+        const voteCount = this.props.rfc.questions.length;
+
+        return <div className="col-md-6">
+            <div className="card">
+                <div className="card-header">
+                    {this.props.rfc.status == 'open' ?
+                        <span className="badge badge-primary mr-1">Active</span>
+                        : null }
+                    <a href={this.props.rfc.url} target="_blank">{this.props.rfc.title}</a>
+                </div>
+                <div className="card-body">
+                    {this.props.rfc.questions.map((item, idx) => {
+                        return <VoteResults question={item.question} results={item.results} share={item.share} last={voteCount == idx+1} />
+                    })}
+                </div>
             </div>
         </div>
     }
@@ -67,7 +73,7 @@ class RfcVoteItem extends React.Component {
 
 class RfcList extends React.Component {
     render () {
-        return <div className="card-columns">
+        return <div className="card-columns row">
             {this.props.rfcs.map(item => { return <RfcVoteItem key={item.id} rfc={item} /> })}
         </div>
     }
