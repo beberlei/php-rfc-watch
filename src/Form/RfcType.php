d17d5f33b496ce61a355a8cb42d7bfa11bac4c7b
Benjamin Eberlei <kontakt@beberlei.de>
2019-11-17 01:10:52 +0100

    Add older versions to form type.

--- src/Form/RfcType.php
+++ src/Form/RfcType.php
@@ -21,7 +21,7 @@ class RfcType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $versions = ['8.0', '7.4', '7.3', '7.2', '7.1', '7.0'];
+        $versions = ['8.0', '7.4', '7.3', '7.2', '7.1', '7.0', '5.6', '5.5', '5.4'];
 
         $builder
             ->add('targetPhpVersion', ChoiceType::class, ['choices' => array_combine($versions, $versions)])
