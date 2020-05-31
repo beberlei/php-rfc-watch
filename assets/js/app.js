c46b17112b83272322c84f1f7b1664344efa9e65
Benjamin Eberlei <kontakt@beberlei.de>
2020-05-31 23:26:22 +0200

    Fix styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -149,7 +149,7 @@ class RfcCommunityVote extends React.Component {
                     </svg>
                     <span className={communityVote.you === 1 ? 'font-bold' : null}>{communityVote.up}</span>
                 </TailwindButton>
-                <TailwindButton className={"rounded-r-md"} onClick={() => this.vote(-1, ctx.logged_in)} title={!ctx.logged_in ? "Login required" : ""} disabled={!ctx.logged_in}>
+                <TailwindButton className={"rounded-r-md -ml-px"} onClick={() => this.vote(-1, ctx.logged_in)} title={!ctx.logged_in ? "Login required" : ""} disabled={!ctx.logged_in}>
                     <svg className={"h-5 w-5"} fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                                       d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5"/>
@@ -172,7 +172,7 @@ class TailwindButton extends React.Component {
         }
 
         return <button type="button" onClick={(ev) => this.props.onClick(ev)} title={this.props.title}
-               className={classes + this.props.className}>
+               className={classes + " " + this.props.className}>
             {this.props.children}
         </button>
     }
