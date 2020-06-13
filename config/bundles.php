322f50d85a2fa03fa309db3c57c10be68e339bc2
Benjamin Eberlei <kontakt@beberlei.de>
2020-06-13 20:42:59 +0200

    Add symfony/mercure-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     QafooLabs\Bundle\NoFrameworkBundle\QafooLabsNoFrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
