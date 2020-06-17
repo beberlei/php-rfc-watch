72473c8369551621cbe4f28281b1213170a5a57d
Benjamin Eberlei <kontakt@beberlei.de>
2020-06-17 12:53:04 +0200

    Update mercure url again

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ Encore
     .enablePostCssLoader()
     .addExternals({
         Config: JSON.stringify({
-            mercureUrl: Encore.isProduction() ? 'https://mercure-public.tideways.io' : 'http://127.0.0.1:3000'
+            mercureUrl: Encore.isProduction() ? 'https://abf7a82a-ea5c-4cbe-920c-3b2b3fb52ee0.mercure.rocks' : 'http://127.0.0.1:3000'
         }),
     })
 ;
