f742e473f60b214c62a0626e3d4df555a290fbb1
Benjamin Eberlei <kontakt@beberlei.de>
2021-06-12 12:06:05 +0200

    Update orm-pack to version 2 for ORM 2.9 and all its dependencies.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
