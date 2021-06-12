28cd85e9521a51b4baffcad89afa0890abae588b
Benjamin Eberlei <kontakt@beberlei.de>
2021-06-12 16:27:59 +0200

    Remove app.js file and from webpack build update node-sass to more recent version.

--- config/bundles.php
+++ config/bundles.php
@@ -12,7 +12,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Gyro\Bundle\MVCBundle\GyroMVCBundle::class => ['all' => true],
 ];
