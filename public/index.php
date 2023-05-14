0288ea698b840e6578dc7f61dddf52953389534b
Benjamin Eberlei <kontakt@beberlei.de>
2023-05-14 19:51:02 +0200

    Rework for pgsql

--- public/index.php
+++ public/index.php
@@ -9,16 +9,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// The check is to ensure we don't use .env in production
-if (! isset($_SERVER['APP_ENV'])) {
-    if (! class_exists(Dotenv::class)) {
-        throw new RuntimeException(
-            'APP_ENV environment variable is not defined. You need to define environment variables ' .
-            'for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.'
-        );
-    }
-
-    (new Dotenv())->load(__DIR__ . '/../.env');
+if (file_exists(__DIR__ . '/../.env')) {
+    (new Dotenv())->load(__DIR__ . '/../.env', );
 }
 
 $env = $_SERVER['APP_ENV'] ?? 'dev';
