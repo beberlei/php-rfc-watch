534ef602c69f81ad32e49d9d576bbe43ec348c70
Benjamin Eberlei <kontakt@beberlei.de>
2023-09-03 21:54:26 +0200

    Convert public/index.php to use Symfony Runtime

--- public/index.php
+++ public/index.php
@@ -3,40 +3,9 @@
 declare(strict_types=1);
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-if (file_exists(__DIR__ . '/../.env')) {
-    (new Dotenv())->load(__DIR__ . '/../.env');
-}
-
-$env = $_SERVER['APP_ENV'] ?? 'dev';
-$debug = (bool) ($_SERVER['APP_DEBUG'] ?? ($env !== 'prod'));
-
-if ($debug) {
-    umask(0000);
-
-    Debug::enable();
-}
-
-$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false;
-if ($trustedProxies !== false) {
-    Request::setTrustedProxies(
-        explode(',', $trustedProxies),
-        Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST
-    );
-}
-
-$trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false;
-if ($trustedHosts !== false) {
-    Request::setTrustedHosts(explode(',', $trustedHosts));
-}
-
-$kernel = new Kernel($env, $debug);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
